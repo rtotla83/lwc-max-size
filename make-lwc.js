5a66f4bc93cead9e0c46a124842ddb95819951d1
minoaw <minoaw@gmail.com>
2020-03-23 18:30:37 +0900

    Add component which has multiple .js file.

--- make-lwc.js
+++ make-lwc.js
@@ -15,3 +15,19 @@ const JS_FILL = `a`.repeat(MAX_SIZE - JS_BASE_LENGTH);
 const js = JS_BASE.replace(PLACEHOLDER, JS_FILL);
 writeFileSync(`force-app/main/default/lwc/lwcMaxSize/lwcMaxSize.js`, js, 'utf8');
 
+
+const IMPORT_BASE1 = `import { LightningElement } from 'lwc';
+import * as jsimport2 from './jsimport2';
+export default class JsImport extends LightningElement {}
+var ${PLACEHOLDER};`;
+const IMPORT_BASE1_LENGTH = IMPORT_BASE1.length - PLACEHOLDER.length;
+const import1 = IMPORT_BASE1.replace(PLACEHOLDER, `a`.repeat(MAX_SIZE - IMPORT_BASE1_LENGTH));
+writeFileSync(`force-app/main/default/lwc/jsImport/jsImport.js`, import1, 'utf8');
+
+const IMPORT_BASE2 = `
+var ${PLACEHOLDER};
+export default {};`;
+const IMPORT_BASE2_LENGTH = IMPORT_BASE2.length - PLACEHOLDER.length;
+const import2 = IMPORT_BASE2.replace(PLACEHOLDER, `a`.repeat(MAX_SIZE - IMPORT_BASE2_LENGTH));
+writeFileSync(`force-app/main/default/lwc/jsImport/jsimport2.js`, import2, 'utf8');
+
